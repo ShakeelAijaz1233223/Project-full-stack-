ae64b4d722b4863e83f63b643763b5df7edf0db9
Sir_Shrukh <Shakeelaijaz559986@gmail.com>
2026-02-07 20:06:41 +0500

    Updates: 2026-02-07T15:06:38.959Z

--- user/user_video_view.php
+++ user/user_video_view.php
@@ -341,15 +341,12 @@ $videos = mysqli_query($conn, $query);
     <span class="artist-tag">Artist: <?= htmlspecialchars($row['artist']) ?></span>
     <span class="album-tag">Album: <?= htmlspecialchars($row['album']) ?></span>
     <span class="year-tag">Year: <?= htmlspecialchars($row['year']) ?></span>
-    <span class="file-tag">File: <?= htmlspecialchars($row['file']) ?></span>
     <span class="album-id-tag">Album ID: <?= htmlspecialchars($row['album_id']) ?></span>
     <span class="genre-tag">Genre: <?= htmlspecialchars($row['genre']) ?></span>
     <span class="duration-tag">Duration: <?= htmlspecialchars($row['duration']) ?></span>
     <span class="desc-tag">Description: <?= htmlspecialchars($row['description']) ?></span>
     <span class="created-tag">Uploaded: <?= date('d M Y', strtotime($row['created_at'])) ?></span>
-    <span class="thumb-tag">Thumbnail: <?= htmlspecialchars($row['thumbnail']) ?></span>
-    <span class="full-thumb-tag">Full Path: <?= htmlspecialchars($row['full_thumbnail_path']) ?></span>
-    <span class="file-path-tag">File Path: <?= htmlspecialchars($row['file_path']) ?></span>
+    
 </div>
 
 
