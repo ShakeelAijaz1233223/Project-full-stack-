751857cf9e5202acd2cce2df9ce9e19990a40156
Sir_Shrukh <Shakeelaijaz559986@gmail.com>
2026-02-07 13:44:28 +0500

    Updates: 2026-02-07T08:44:26.665Z

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -334,7 +334,7 @@ $userName = !empty($userData['name']) ? $userData['name'] : 'Admin';
             <a href="add_music.php"><i class="fa-solid fa-cloud-arrow-up"></i> Add Music</a>
             <a href="add_video.php"><i class="fa-solid fa-file-video"></i> Add Video</a>
             <a href="music_reviews.php"><i class="fa-solid fa-envelope-open-text"></i> Music_Review</a>
-            <a href="video_reviews.php"><i class="fa-solid fa-envelope-open-text"></i> Video_Review</a>
+            <a href="video_review.php"><i class="fa-solid fa-envelope-open-text"></i> Video_Review</a>
             <a href="admin_messages.php"><i class="fa-solid fa-envelope-open-text"></i> Messages</a>
            
             <a href="logout.php" class="text-danger mt-3"><i class="fa-solid fa-power-off"></i> Sign Out</a>
