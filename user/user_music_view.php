03e1b61ea4eec10b6049d07f6e86ee339b4da5f4
Sir_Shrukh <Shakeelaijaz559986@gmail.com>
2026-02-07 17:32:21 +0500

    Updates: 2026-02-07T12:32:18.844Z

--- user/user_music_view.php
+++ user/user_music_view.php
@@ -104,7 +104,7 @@ footer { text-align:center; padding:40px; font-size:0.7rem; color:#444; }
             <div class="disc-wrapper">
                 <i class="bi bi-disc-fill text-muted" style="font-size:4rem;"></i>
                 <button class="play-btn"><i class="bi bi-play-fill"></i></button>
-                <img src="" alt="">
+                <img src="https://stock.adobe.com/search?k=music" alt="">
             </div>
             <div class="custom-controls">
                 <input type="range" class="progress" min="0" max="100" value="0">
