e3baaa126ce1678c970ba19a008f49f5663f2aa4
Sir_Shrukh <Shakeelaijaz559986@gmail.com>
2026-02-07 16:14:57 +0500

    Updates: 2026-02-07T11:14:55.703Z

--- user/user_albums_view.php
+++ user/user_albums_view.php
@@ -158,8 +158,8 @@ $albums = mysqli_query($conn, $query);
 
 /* Play button centered */
 .album-card .play-btn {
-    position: static;
-    top: 50%;
+    position: sticky;
+    top: 20%;
     left: 50%;
     transform: translate(-50%, -50%);
     background: rgba(255, 0, 85, 0.3);
